16ba9ba7bb3a3d3e2759ad9c76c4afc1e249836f
Flounder <testy@test.com>
2020-03-25 16:58:29 -0500

    Inspiration List Fragment Setup & list item XML created

--- app/src/main/java/com/carson/androidinsperationboard/InsperationList.java
+++ app/src/main/java/com/carson/androidinsperationboard/InsperationList.java
@@ -1,10 +1,12 @@
 package com.carson.androidinsperationboard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,6 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 public class InsperationList extends Fragment {
@@ -20,30 +26,12 @@ public class InsperationList extends Fragment {
     private EditText mSearchHashtagEntry;
     private RecyclerView mInspirationList;
 
+    //TODO need adapter component
+
     public InsperationList() {
         // Required empty public constructor
     }
 
-    /*
-    // TODO: Rename and change types and number of parameters
-    public static InsperationList newInstance(String param1, String param2) {
-        InsperationList fragment = new InsperationList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -52,8 +40,7 @@ public class InsperationList extends Fragment {
         mSearchButton = v.findViewById(R.id.search_button);
         mSearchHashtagEntry = v.findViewById(R.id.search_hashtags_entry);
         mInspirationList = v.findViewById(R.id.inspiration_list);
-
-
+        mInspirationList.setLayoutManager(new LinearLayoutManager(getActivity()));
         mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,28 +48,83 @@ public class InsperationList extends Fragment {
                 //search hastags
             }
         });
-
-
-        // Inflate the layout for this fragment
+        updateUI();
         return v;
     }
 
-
-    /*
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onResume(){
+        super.onResume();
+         updateUI();
+    }
+
+    private void updateUI() {
+        //get inpirationLab class to getActivity
+        //create liste List<Inspiration> inspirations = inspirationLab.getInspirations();
+
+        if (mAdapter == null) {
+            mAdapter = new InspirationAdapter(inspirations);
+            mInspirationList.setAdapter(mAdapter);
+        }else{
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class InspirationHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private TextView mInspirationText;
+        private TextView mInspirationHashtag;
+        private TextView mInspirationDate;
+        private ImageView mInspirationImage;
+
+        private InspirationRecord mInspirationRecord;
+
+        public InspirationHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_inspiration, parent, false));
+            itemView.setOnClickListener(this);
+            mInspirationText = itemView.findViewById(R.id.inspiration_text);
+            mInspirationHashtag = itemView.findViewById(R.id.inspiration_hashtag);
+            mInspirationDate = itemView.findViewById(R.id.inspiration_date);
+            mInspirationImage = itemView.findViewById(R.id.inspiration_image);
+        }
+
+        public void bind (InspirationRecord inspirationRecord){
+            mInspirationRecord = inspirationRecord;
+
+            mInspirationText.setText(mInspirationRecord.getInspiration());
+            mInspirationHashtag.setText(mInspirationRecord.getHashtag());
+            mInspirationDate.setText(mInspirationRecord.getDate());
+            mInspirationImage.setImage(mInspirationRecord.getImage());
+
+        }
+
+        @Override
+        public void onClick (View v) {
+        Intent intent = MainActivity.newIntent(getActivity(), mInspirationRecord.getId());
+        startActivity(intent);
+        }
+    }
+
+    private class InspirationAdapter extends RecyclerView.Adapter<InspirationHolder> {
+        private List<InspirationRecord> mInspirationRecord;
+
+        public InspirationAdapter (List<InspirationRecord> inspirations) {mInspirationRecord = inspirations;}
+
+        @Override
+        public InspirationHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new InspirationHolder(layoutInflater, parent);
         }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }*/
+    public void onBindViewHolder(InspirationHolder holder, int position){
+        InspirationRecord inspirationRecord = mInspirationRecord.get(position);
+        holder.bind(inspirationRecord);
+    }
+
+    @Override
+    public int getItemCount() {return mInspirationRecord.size();}
+
 }
--- app/src/main/java/com/carson/androidinsperationboard/InspirationRecord.java
+++ app/src/main/java/com/carson/androidinsperationboard/InspirationRecord.java
@@ -46,6 +46,8 @@ public class InspirationRecord {
         this.hashtag = hashtag;
     }
 
+    //image
+
     @Override
     public String toString(){
         return "InspirationRecord{" +
